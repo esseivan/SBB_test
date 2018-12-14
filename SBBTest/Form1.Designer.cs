21560d2e0eec3c72ea0dc9af5855a7ddea0263bd
EsseivaN <nicolas.esseiva@gmail.com>
2018-12-14 23:26:07 +0100

    Initial upload

---
+++ SBBTest/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace SBBTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxFrom = new System.Windows.Forms.TextBox();
+            this.btnFrom = new System.Windows.Forms.Button();
+            this.chblFrom = new System.Windows.Forms.CheckedListBox();
+            this.chblTo = new System.Windows.Forms.CheckedListBox();
+            this.tbxTo = new System.Windows.Forms.TextBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.chbStart = new System.Windows.Forms.CheckBox();
+            this.chbEnd = new System.Windows.Forms.CheckBox();
+            this.libConn = new System.Windows.Forms.ListBox();
+            this.txtConn = new System.Windows.Forms.RichTextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnFromDel = new System.Windows.Forms.Button();
+            this.btnToDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxFrom
+            // 
+            this.tbxFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxFrom.Location = new System.Drawing.Point(12, 383);
+            this.tbxFrom.Name = "tbxFrom";
+            this.tbxFrom.Size = new System.Drawing.Size(216, 20);
+            this.tbxFrom.TabIndex = 2;
+            this.tbxFrom.Text = "Fétigny";
+            this.tbxFrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnFrom
+            // 
+            this.btnFrom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFrom.Location = new System.Drawing.Point(12, 409);
+            this.btnFrom.Name = "btnFrom";
+            this.btnFrom.Size = new System.Drawing.Size(105, 23);
+            this.btnFrom.TabIndex = 3;
+            this.btnFrom.Text = "Affichre";
+            this.btnFrom.UseVisualStyleBackColor = true;
+            this.btnFrom.Click += new System.EventHandler(this.btnFrom_Click);
+            // 
+            // chblFrom
+            // 
+            this.chblFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.chblFrom.FormattingEnabled = true;
+            this.chblFrom.Location = new System.Drawing.Point(12, 12);
+            this.chblFrom.Name = "chblFrom";
+            this.chblFrom.Size = new System.Drawing.Size(216, 364);
+            this.chblFrom.Sorted = true;
+            this.chblFrom.TabIndex = 4;
+            this.chblFrom.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chblFrom_ItemCheck);
+            // 
+            // chblTo
+            // 
+            this.chblTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.chblTo.FormattingEnabled = true;
+            this.chblTo.Location = new System.Drawing.Point(234, 12);
+            this.chblTo.Name = "chblTo";
+            this.chblTo.Size = new System.Drawing.Size(216, 364);
+            this.chblTo.Sorted = true;
+            this.chblTo.TabIndex = 4;
+            this.chblTo.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.chblTo_ItemCheck);
+            // 
+            // tbxTo
+            // 
+            this.tbxTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxTo.Location = new System.Drawing.Point(234, 383);
+            this.tbxTo.Name = "tbxTo";
+            this.tbxTo.Size = new System.Drawing.Size(216, 20);
+            this.tbxTo.TabIndex = 2;
+            this.tbxTo.Text = "Payerne";
+            this.tbxTo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnTo.Location = new System.Drawing.Point(234, 409);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(105, 23);
+            this.btnTo.TabIndex = 3;
+            this.btnTo.Text = "Afficher";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.CustomFormat = "dd.MM.yyyy  |  hh:mm";
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDate.Location = new System.Drawing.Point(456, 12);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 5;
+            // 
+            // chbStart
+            // 
+            this.chbStart.AutoSize = true;
+            this.chbStart.Checked = true;
+            this.chbStart.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbStart.Location = new System.Drawing.Point(456, 38);
+            this.chbStart.Name = "chbStart";
+            this.chbStart.Size = new System.Drawing.Size(59, 17);
+            this.chbStart.TabIndex = 6;
+            this.chbStart.Text = "Arrivée";
+            this.chbStart.UseVisualStyleBackColor = true;
+            this.chbStart.CheckedChanged += new System.EventHandler(this.chbStart_CheckedChanged);
+            // 
+            // chbEnd
+            // 
+            this.chbEnd.AutoSize = true;
+            this.chbEnd.Location = new System.Drawing.Point(598, 38);
+            this.chbEnd.Name = "chbEnd";
+            this.chbEnd.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.chbEnd.Size = new System.Drawing.Size(58, 17);
+            this.chbEnd.TabIndex = 6;
+            this.chbEnd.Text = "Départ";
+            this.chbEnd.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.chbEnd.UseVisualStyleBackColor = true;
+            this.chbEnd.CheckedChanged += new System.EventHandler(this.chbEnd_CheckedChanged);
+            // 
+            // libConn
+            // 
+            this.libConn.FormattingEnabled = true;
+            this.libConn.Location = new System.Drawing.Point(456, 61);
+            this.libConn.Name = "libConn";
+            this.libConn.Size = new System.Drawing.Size(200, 316);
+            this.libConn.TabIndex = 7;
+            // 
+            // txtConn
+            // 
+            this.txtConn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConn.Location = new System.Drawing.Point(662, 12);
+            this.txtConn.Name = "txtConn";
+            this.txtConn.ReadOnly = true;
+            this.txtConn.Size = new System.Drawing.Size(338, 365);
+            this.txtConn.TabIndex = 8;
+            this.txtConn.Text = "";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(456, 409);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(200, 23);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Rafraîchir";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnFromDel
+            // 
+            this.btnFromDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFromDel.Location = new System.Drawing.Point(123, 409);
+            this.btnFromDel.Name = "btnFromDel";
+            this.btnFromDel.Size = new System.Drawing.Size(105, 23);
+            this.btnFromDel.TabIndex = 3;
+            this.btnFromDel.Text = "Effacer";
+            this.btnFromDel.UseVisualStyleBackColor = true;
+            this.btnFromDel.Click += new System.EventHandler(this.btnFromDel_Click);
+            // 
+            // btnToDel
+            // 
+            this.btnToDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnToDel.Location = new System.Drawing.Point(345, 409);
+            this.btnToDel.Name = "btnToDel";
+            this.btnToDel.Size = new System.Drawing.Size(105, 23);
+            this.btnToDel.TabIndex = 3;
+            this.btnToDel.Text = "Effacer";
+            this.btnToDel.UseVisualStyleBackColor = true;
+            this.btnToDel.Click += new System.EventHandler(this.btnToDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtConn);
+            this.Controls.Add(this.libConn);
+            this.Controls.Add(this.chbEnd);
+            this.Controls.Add(this.chbStart);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.chblTo);
+            this.Controls.Add(this.chblFrom);
+            this.Controls.Add(this.btnToDel);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.btnFromDel);
+            this.Controls.Add(this.btnFrom);
+            this.Controls.Add(this.tbxTo);
+            this.Controls.Add(this.tbxFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbxFrom;
+        private System.Windows.Forms.Button btnFrom;
+        private System.Windows.Forms.CheckedListBox chblFrom;
+        private System.Windows.Forms.CheckedListBox chblTo;
+        private System.Windows.Forms.TextBox tbxTo;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.CheckBox chbStart;
+        private System.Windows.Forms.CheckBox chbEnd;
+        private System.Windows.Forms.ListBox libConn;
+        private System.Windows.Forms.RichTextBox txtConn;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnFromDel;
+        private System.Windows.Forms.Button btnToDel;
+    }
+}
+
---
+++ SBBTest/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+
+namespace SBBTest
+{
+    public partial class Form1 : Form
+    {
+        SBB_Serializer sbb_serializer = new SBB_Serializer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnFrom_Click(object sender, EventArgs e)
+        {
+            SBB_Serializer.Stations stations = sbb_serializer.getStations(tbxFrom.Text);
+            string[] names = sbb_serializer.getStationNames(stations);
+            chblFrom.Items.AddRange(names);
+        }
+
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+            SBB_Serializer.Stations stations = sbb_serializer.getStations(tbxTo.Text);
+            string[] names = sbb_serializer.getStationNames(stations);
+            chblTo.Items.AddRange(names);
+        }
+
+        private void chblFrom_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int ix = 0; ix < chblFrom.Items.Count; ++ix)
+            {
+                if (ix != e.Index)
+                {
+                    chblFrom.SetItemChecked(ix, false);
+                }
+            }
+        }
+
+        private void chblTo_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            for (int ix = 0; ix < chblTo.Items.Count; ++ix)
+            {
+                if (ix != e.Index)
+                {
+                    chblTo.SetItemChecked(ix, false);
+                }
+            }
+        }
+
+        private void chbStart_CheckedChanged(object sender, EventArgs e)
+        {
+            bool c1 = chbStart.Checked,
+                c2 = chbEnd.Checked;
+            if (c1 && c2)
+            {
+                chbEnd.Checked = false;
+            }
+            else if (!c1 && !c2)
+            {
+                chbEnd.Checked = true;
+            }
+        }
+
+        private void chbEnd_CheckedChanged(object sender, EventArgs e)
+        {
+            bool c1 = chbEnd.Checked,
+                c2 = chbStart.Checked;
+            if (c1 && c2)
+            {
+                chbStart.Checked = false;
+            }
+            else if (!c1 && !c2)
+            {
+                chbStart.Checked = true;
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            sbb_serializer.UpdateIsArrivalTime(chbEnd.Checked);
+            SBB_Serializer.Connections connections = sbb_serializer.getConnections(chblFrom.SelectedItem.ToString(), chblTo.SelectedItem.ToString(), dtpDate.Value);
+            string[] names = sbb_serializer.getConnectionsNames(connections);
+            libConn.Items.AddRange(names);
+            string t = sbb_serializer.displayConnections(connections);
+            txtConn.Text = t;
+        }
+
+        private void btnFromDel_Click(object sender, EventArgs e)
+        {
+            chblFrom.Items.Clear();
+        }
+
+        private void btnToDel_Click(object sender, EventArgs e)
+        {
+            chblTo.Items.Clear();
+        }
+    }
+
+    public class SBB_Serializer
+    {
+        bool isArrivalTime = false;
+
+        // Main
+        public SBB_Serializer()
+        {
+            if (Directory.Exists(@"C:\temp"))
+            {
+                Directory.CreateDirectory(@"C:\temp");
+            }
+        }
+
+        private string readWebData(string url)
+        {
+            Console.WriteLine("Downloading data from : \"" + url + "\"");
+            string data = string.Empty;
+            using (WebClient wc = new WebClient())
+            {
+                data = wc.DownloadString(url);
+            }
+
+            return data;
+        }
+
+        // Stations
+
+        public Stations getStations(string query)
+        {
+            return getStations(query, string.Empty, string.Empty, string.Empty);
+        }
+
+        public Stations getStations(string query, string x)
+        {
+            return getStations(query, x, string.Empty, string.Empty);
+        }
+
+        public Stations getStations(string query, string x, string y)
+        {
+            return getStations(query, x, y, string.Empty);
+        }
+
+        public Stations getStations(string query, string x, string y, string type)
+        {
+            Console.WriteLine("Working...");
+            string data = readWebData("https://transport.opendata.ch/v1/locations?"
+                + (query == string.Empty ? "" : "query=" + query + "&")
+                + (x == string.Empty ? "" : "x=" + x + "&")
+                + (y == string.Empty ? "" : "y=" + y + "&")
+                + (type == string.Empty ? "" : "type=" + y));
+
+
+            File.WriteAllText(@"C:\temp\jsonData.json", data);
+            StreamReader sr = new StreamReader(@"C:\temp\jsonData.json");
+
+            System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Stations));
+            Stations stations = serializer.ReadObject(sr.BaseStream) as Stations;
+            sr.Dispose();
+            Console.WriteLine("Complete !");
+            return stations;
+        }
+
+        public string displayStations(Stations stations)
+        {
+            string output = string.Empty;
+            if (stations != null)
+            {
+                foreach (var item in stations.stations)
+                {
+                    output += "#######################" + Environment.NewLine;
+                    output += "Station : " + item.name + Environment.NewLine;
+                    output += "Id : " + item.id + Environment.NewLine;
+                    output += "score : " + item.score + Environment.NewLine;
+                    output += "coordinate : " + Environment.NewLine;
+                    output += "\ttype : " + item.coordinate.type + Environment.NewLine;
+                    output += "\tx : " + item.coordinate.x + Environment.NewLine;
+                    output += "\ty : " + item.coordinate.y + Environment.NewLine;
+                }
+            }
+            return output;
+        }
+
+        public string[] getStationNames(Stations stations)
+        {
+            string[] names = new string[stations.stations.Length];
+            for (int i = 0; i < stations.stations.Length; i++)
+            {
+                names[i] = stations.stations[i].name;
+            }
+            return names;
+        }
+
+        [DataContract]
+        public class Stations
+        {
+            [DataMember]
+            public Station[] stations;
+
+            [DataContract]
+            public class Station
+            {
+                [DataMember]
+                public string id;
+                [DataMember]
+                public string name;
+                [DataMember]
+                public string score;
+                [DataMember]
+                public Coordinate coordinate;
+                [DataMember]
+                public string distance;
+
+                [DataContract]
+                public class Coordinate
+                {
+                    [DataMember]
+                    public string type;
+                    [DataMember]
+                    public object x;
+                    [DataMember]
+                    public object y;
+                }
+            }
+        }
+
+        // Connections
+
+        public void UpdateIsArrivalTime(bool newValue)
+        {
+            isArrivalTime = newValue;
+        }
+
+        public Connections getConnections(string from)
+        {
+            return getConnections(from, string.Empty, DateTime.Now);
+        }
+
+        public Connections getConnections(string from, string to)
+        {
+            return getConnections(from, to, DateTime.Now);
+        }
+
+        public Connections getConnections(string from, string to, DateTime date)
+        {
+            Console.WriteLine("Working...");
+            string data = readWebData("https://transport.opendata.ch/v1/connections?"
+                + (from == string.Empty ? "" : "from=" + from + "&")
+                + (to == string.Empty ? "" : "to=" + to + "&")
+                + (date == null ? "" : "date=" + date.ToString("yyyy-MM-dd") + "&")
+                + (date == null ? "" : "time=" + date.ToString("hh:mm") + "&")
+                + "isArrivalTime=" + (isArrivalTime ? "1" : "0"));
+
+            File.WriteAllText(@"C:\temp\jsonData.json", data);
+            StreamReader sr = new StreamReader(@"C:\temp\jsonData.json");
+
+            System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(Connections));
+            Connections connections = serializer.ReadObject(sr.BaseStream) as Connections;
+            sr.Dispose();
+            Console.WriteLine("Complete !");
+            return connections;
+        }
+
+        public string displayConnections(Connections connections)
+        {
+            string output = string.Empty;
+            if (connections != null)
+            {
+                foreach (var item in connections.connections)
+                {
+                    output += "#######################" + Environment.NewLine;
+                    output += "Connection : " + Environment.NewLine;
+                    output += "From : " + item.from.station.name.ToString() + Environment.NewLine;
+                    output += "At : " + item.from.departure.ToString() + Environment.NewLine;
+                    output += "To : " + item.to.station.name.ToString() + Environment.NewLine;
+                    output += "At : " + item.to.arrival.ToString() + Environment.NewLine;
+                    output += "Duration : " + item.duration.ToString() + Environment.NewLine;
+                }
+            }
+            return output;
+        }
+
+        public string[] getConnectionsNames(Connections connections)
+        {
+            string[] names = new string[connections.connections.Length];
+            for (int i = 0; i < connections.connections.Length; i++)
+            {
+                names[i] = connections.connections[i].from.station.name + " ||| " + connections.connections[i].to.station.name;
+            }
+            return names;
+        }
+
+        [DataContract]
+        public class Connections
+        {
+            [DataMember]
+            public Connection[] connections;
+
+            [DataContract]
+            public class Connection
+            {
+                [DataMember]
+                public ConnectionDetails from;
+                [DataMember]
+                public ConnectionDetails to;
+                [DataMember]
+                public string duration;
+                [DataMember]
+                public int transfers;
+                [DataMember]
+                public object service;
+                [DataMember]
+                public object products;
+                [DataMember]
+                public object capacity1st;
+                [DataMember]
+                public object capacity2st;
+                [DataMember]
+                public object sections;
+
+                [DataContract]
+                public class ConnectionDetails
+                {
+                    [DataMember]
+                    public Stations.Station station;
+                    [DataMember]
+                    public object arrival;
+                    [DataMember]
+                    public object arrivalTimestamp;
+                    [DataMember]
+                    public string departure;
+                    [DataMember]
+                    public object departureTimestamp;
+                    [DataMember]
+                    public object delay;
+                    [DataMember]
+                    public object platform;
+                    [DataMember]
+                    public object prognosis;
+                    [DataMember]
+                    public object realtimeAvailability;
+                    [DataMember]
+                    public Stations.Station location;
+
+                    [DataContract]
+                    public class Prognosis
+                    {
+                        [DataMember]
+                        public object platform;
+                        [DataMember]
+                        public object arrival;
+                        [DataMember]
+                        public object departure;
+                        [DataMember]
+                        public object capacity1st;
+                        [DataMember]
+                        public object capacity2nd;
+                    }
+                }
+
+                [DataContract]
+                public class Sections
+                {
+                    [DataMember]
+                    public object journey;
+                    [DataMember]
+                    public object walk;
+                    [DataMember]
+                    public ConnectionDetails departure;
+                    [DataMember]
+                    public ConnectionDetails arrival;
+
+                    [DataContract]
+                    public class Walk
+                    {
+                        [DataMember]
+                        public string duration;
+                    }
+                }
+            }
+        }
+    }
+}
